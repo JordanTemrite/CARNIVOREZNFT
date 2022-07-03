29e27383ffbaea488f5d4649c95eee6562952667
Zada BSC <83736116+ZadaUniverse@users.noreply.github.com>
2022-07-03 02:11:52 -0400

    Update Pill.sol

--- PILLS/Pill.sol
+++ PILLS/Pill.sol
@@ -10,17 +10,17 @@ import "./Address.sol";
 contract Pill is ERC721A, Ownable {
     using Address for address;
 
-    IERC721 cz = IERC721(0x48926596Eac835dd906c1694FE620f75fB2F588b);
+    IERC721 cz = IERC721(0x79182905C2F78787F2e194378979E9379e6A9EE7);
+
     address constant cz_deployer = payable(0xdFf3692F88123163f37c62f79eDE55Fccc58Ade5);
 
     uint256 constant max_supply = 5000;
     uint256 public min_purchase_price = .03 ether;
     uint256 public total_burned;
 
-    bool internal burn_enabled = true;
+    bool internal burn_enabled = false;
     bool public mint_state = false;
 
-    mapping(address => uint256) public available_mints;
     mapping(address => bool) public trusted;
 
     constructor() ERC721A("TEST", "TST") {
@@ -41,34 +41,29 @@ contract Pill is ERC721A, Ownable {
     }
 
     function set_trusted(address _trusted, bool _state) external onlyOwner {
+
         trusted[_trusted] = _state;
     }
 
-    function test_transfer(uint256 _tokenID) external {
-        cz.transferFrom(msg.sender, address(this), _tokenID);
-    }
-
-    function burn_cz(uint256 _tokenID) payable external {
-
-        require(cz.ownerOf(_tokenID) == msg.sender, "CZ : YOU DO NOT OWN THIS NFT");
+    function burn_cz(uint256[] calldata _tokenID) payable external {
 
         require(mint_state, "CZ : MINTING IS NOT ENABLED");
-        require(msg.value >= min_purchase_price, "CZ : INSUFFCIENT VALUE SENT");
-        require(payable(cz_deployer).send(msg.value), "CZ: ETHER MUST BE SENT TO THE DEPLOYER VIA MINT FUNCTION");
+        require(msg.value >= (min_purchase_price * _tokenID.length), "CZ : INSUFFCIENT VALUE SENT");
+        require(payable(cz_deployer).send(msg.value), "CZ: ETHER MUST BE SENT TO THE DEPLOYER");
         require(totalSupply() + 1 <= max_supply, "CZ : ATTEMPTING TO MINT PAST MAX");
 
-        cz.transferFrom(msg.sender, address(this), _tokenID);
+        for(uint k = 0; k < _tokenID.length; k++) {
+            cz.transferFrom(msg.sender, 0x000000000000000000000000000000000000Cdad, _tokenID[k]);
+        }
 
-        total_burned += 1;
+        total_burned += _tokenID.length;
 
-        _safeMint(msg.sender, 1);
+        _safeMint(msg.sender, _tokenID.length);
     }
 
     function burn_pill(uint256[] calldata _tokenID) external {
 
-        for(uint i = 0; i < _tokenID.length; i ++) {
-            require(ownerOf(_tokenID[i]) == msg.sender || trusted[msg.sender] == true, "CZ : YOU DO NOT OWN THIS PILL");
-        }
+        require(trusted[msg.sender] == true, "CZ : YOU ARE NOT TRUSTED BY CZ");
 
         require(burn_enabled, "CZ : PILL BURNING IS DISABLED");
 
